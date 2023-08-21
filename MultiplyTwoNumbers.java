e590f74b6e598fac8beb9514657dfd2147458396
mypc <waghgauri68@gmail.com>
2023-08-21 23:08:06 +0530

    first commit;

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,15 @@
+package Program;
+
+import java.util.Scanner;
+
+public class MultiplyTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int num1=sc.nextInt();
+        System.out.println("Enter the second number");
+        int num2=sc.nextInt();
+         double result=num1*num2;
+        System.out.println("RESULT:"+result);
+    }
+}
